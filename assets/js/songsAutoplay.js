0e09589f2c741e4461d0655092f6d33be5f274d3
Emiliano Dichter <emiliano.dichter@gmail.com>
2025-06-24 13:42:32 +0200

    added autoplay feature to songs

---
+++ assets/js/songsAutoplay.js
@@ -0,0 +1,22 @@
+const songs = [...document.querySelectorAll('.song')];
+
+songs.forEach(song => song.ontimeupdate = nextSong);
+    
+function nextSong(e) {
+
+    const end = this.duration;
+
+    let now = this.currentTime;
+
+    if (end <= now) {
+        
+    const position = songs.indexOf(this);
+
+    if (position === songs.length - 1) {
+        songs[0].play();
+
+    } else {
+        songs[position + 1].play();
+    }
+}
+}
